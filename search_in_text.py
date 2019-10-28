6cadbc39537ddd998e35648e25fd88417aa08996
Smbat Makiyan <simfeo@yandex.ru>
2019-10-28 18:18:53 +0200

    searching words in text

--- search_in_text.py
+++ search_in_text.py
@@ -1,9 +1,41 @@
 import os
 import sys
+import re
+import collections
 
 if __name__ == '__main__':
-    path_for_file = sys.argv[1]
-    f = open('')
-    
+    #file_name = sys.argv[1]
+   
+    file_name = r'd:\Shagane\FirstProject\oscarwilde.txt'
+    # f = open(file_name)
+    read_file = open(file_name).read()
+    all_words = re.findall(r'[A-Za-z]+-?[\']?[a-z]+', read_file)
+    words_quantity = len(all_words)
+    print('1 METHOD: There are', words_quantity, 'words in the text')
+
+    long_words = []
+    words_quantity2 = 0
+
+    for word in all_words:
+        words_quantity2 += 1
+        # print (word)
+
+        if len(word) >= 3:
+            long_words.append(word)
+
+    resalt_counting = collections.Counter(all_words)
+  
+    print('2 METHOD: There are', words_quantity2, 'words in the text')
+
+    print('10 all most common words in the text are: ', resalt_counting.most_common(10)) 
+
+    print('10 long most common words are : ', collections.Counter(long_words).most_common(10))
+
+
+
+            
+            
+        
+
 
 
