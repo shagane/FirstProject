1279fe5be114beaf3533b69d501ca4c90ff625d3
Smbat Makiyan <simfeo@yandex.ru>
2019-11-03 21:55:04 +0200

    import urllib.request

--- url_re.py
+++ url_re.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*- 
-from urllib import request
+import urllib.request
 import re
+
+#vfrom urllib import request
 
 some_url = "http://lenta.ru/news/2015/04/26/terrorism/"
 
-filehandle = request.urlopen(some_url)
+filehandle = urllib.request.urlopen(some_url)
 
 html_code = filehandle.read()
 
@@ -12,8 +14,8 @@ links_pattern = "<a.*?</a>"
 
 links = re.findall(links_pattern, str(html_code))
 
-# for iiii in links:
-# 	print (iiii)
+for iiii in links:
+    print (iiii)
 
 import sys
 print("😊😊😊😊")
