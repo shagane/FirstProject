811a5b16ad08c407814ea98ca50ae8bf6f20debc
Smbat Makiyan <simfeo@yandex.ru>
2019-12-09 20:05:06 +0200

    strip text pattern with re sub function

--- html-filter.py
+++ html-filter.py
@@ -18,45 +18,45 @@ def get_data():
 #         text = input_file.read()
 #     return text
 
-def parse_items_from_shop(text):
+def parse_items_intext(text):
     patterns = [r'"articul":"\w*"', r'"brand":"[^"]+"', r'"pn":"[^"]+"', r'"p_price":"\d+.00"', r'"p_original_price":"\d+.00"']
     items = []
     for pattern in patterns:
         item = re.findall(pattern, text)
+        item = re.sub(r'("\w+":")'r'([^"]+)'r'(")', r'\2', str(item))
         items.append(item)
+    return items
 
-    articul, brand, pn, p_price, p_original_price = [x for x in items]
-    return articul, brand, pn, p_price, p_original_price
 
-# def tag_separation(items):
-#     for i in range(len(items)):
-#         articul, brand, pn, p_price, p_original_price = items[i]
-#     return articul, brand, pn, p_price, p_original_price
+# class DataBase():
+#     def __init__(self, name, articul, brand, prod_name, price, original_price):
+#         self.name = name
+#         self.values = values
 
-# def create_db(db_name):
-#     mydb = sqlite3.connect('make-up_items.db')
-#     cursor = mydb.cursor()
-#     cursor.execute('''CREATE TABLE {}
+#     def create_db(self):
+#         mydb = sqlite3.connect('make-up_items.db')
+#         cursor = mydb.cursor()
+#         cursor.execute('''CREATE TABLE IF NOT EXISTS {}
 #                         (articul, brand, pn, p_price, p_original_price)
-#                         '''.format(db_name))
-#     return mydb
-    
-# def push_item(articul, brand, pn, p_price, p_original_price, db=create_db):     
-#     db.execute("""INSERT INTO {} VALUES
-#                     ({}, {}, {}, {}, {})
-#                     """.format(articul, brand, pn, p_price, p_original_price))
-    
-
+#                         '''.format(self.name))
+        
+#     def push_item(articul, brand, pn, p_price, p_original_price, db_name, db=):     
+#         db.execute("""INSERT INTO {} VALUES
+#                         ({}, {}, {}, {}, {})
+#                         """.format(db_name, articul, brand, pn, p_price, p_original_price))
+        
+        
 def main():
     html_text = get_data()
-    items_list = parse_items_from_shop(html_text)
-    # db_name = "Make-up items"
+    items = parse_items_intext(html_text)
+    articul, brand, prod_name, price, original_price = [x for x in items]
+    db_name = 'Makeup_items'
+    make_up_db = create_db(db_name)
     
-    # create_db(db_name)
 
+        
 if __name__ == "__main__":
     main()
-
 
 
 # with open(r'd:\Shagane\FirstProject\test.html', 'w') as output_file:
