c40eaf37c60a2d1bb15d46c29472ca636e47e7b9
shagane <shaganeshka13@gmail.com>
2019-12-10 20:16:29 +0200

    class DB replaced with functions

--- html-filter.py
+++ html-filter.py
@@ -8,51 +8,50 @@ def get_data():
     r = requests.get(url)
     return r.text.encode('utf-8').decode('raw-unicode-escape')
 
-# def contain_items(text):
-#     soup = BeautifulSoup(text)
-#     item_list = soup.find()
-#     return item_list is not None
-
 def parse_items_intext(text):
     patterns = r'''\"articul\":\"(\w*)\",\"brand\":\"([^"]+)\",\"pn\":\"(.*?)\",\"p_price\":\"(\d+.00)\",\"p_original_price\":\"(\d+.00)\"'''
     items = re.findall(patterns, text)
     return items
 
-class DataBase():
+def conn(db_name):
+    conn = sqlite3.connect('{}.db'.format(db_name))
+    return conn
 
-    def __init__(self, name, items):
-        self.name = name
-        self.items = items
-        self.conn = sqlite3.connect('{}.db'.format(self.name))
-        
-    def create_db(self):
-        cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS {}
-                        (articul text, 
-                        brand text,
-                        pn text, 
-                        price text, 
-                        original_price text)
-                        '''.format(self.name))
-        self.conn.commit
-        return cursor.lastrowid
+def create_db(conn, db_name):
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS {}
+                    (Articul text PRIMARY KEY, 
+                    Brand text,
+                    Product_name text, 
+                    Price text, 
+                    Original_price text)
+                    '''.format(db_name))
+    conn.commit
+    cursor.lastrowid
+    return conn
    
-    def insert_item(self):
-        cursor = self.conn.cursor()
-        cursor.executemany("""INSERT INTO {} VALUES
+def insert_items(conn, db_name, items):
+    for i in range(len(items)):
+        conn.cursor().execute("""INSERT INTO {} VALUES
                     (?, ?, ?, ?, ?)
-                    """.format(self.name), self.items)
-        self.conn.commit
-        return cursor.lastrowid
+                    """.format(db_name), items[i])
+        conn.commit
+    return conn
+
+def select_query(conn, db_name, atr):
+    select_query = """SELECT * FROM {} WHERE Articul=?""".format(db_name)
+    conn.cursor().execute(select_query, atr)
+    select = conn.cursor().fetchall
+    return select
         
-              
 def main():
     html_text = get_data()
     items = parse_items_intext(html_text)
     db_name = 'Makeup_items'
-    make_up_db = DataBase(db_name, items)
-    make_up_db.create_db()
-    make_up_db.insert_item()
+    db_connection = conn(db_name)
+    dbcreated = create_db(db_connection, db_name)
+    insert_items(dbcreated, db_name, items)
+   
 
         
 if __name__ == "__main__":
